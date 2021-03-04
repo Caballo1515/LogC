37edaac4a43e7a53bd6102e764ff6be60ca5008e
Alexandru Kitsenco <64800754+alexandrukitsenco@users.noreply.github.com>
2021-03-04 13:01:17 +0100

    añadiendo cosas al log, lo acabare despues de la partidita al lol

--- cat.urv.deim.asm.labs/Log.java
+++ cat.urv.deim.asm.labs/Log.java
@@ -1,3 +1,27 @@
 public class Log extends ConsoleLog {
 
+    public Log(){
+
+    }
+
+    public void error(String tag, String message){
+        System.out.println(ANSI_RED + tag + " - " + message + ANSI_RESET);
+    }
+
+    public void warring(String tag, String message){
+        System.out.println(ANSI_YELLOW + tag + " - " + message + ANSI_RESET);
+    }
+
+    public void debug(String tag, String message){
+        System.out.println(ANSI_BLUE + tag + " - " + message + ANSI_RESET);
+    }
+
+    public void info(String tag, String message){
+        System.out.println(ANSI_BLACK + tag + " - " + message + ANSI_RESET);
+    }
+
+    public void verbose(String tag, String message){
+        System.out.println(ANSI_BLACK + tag + " - " + message + ANSI_RESET);
+    }
+
 }
--- cat.urv.deim.asm.labs/Main.java
+++ cat.urv.deim.asm.labs/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Chinga tu madre");
-	ConsoleLog c= new ConsoleLog();
+	Log c= new Log();
 	c.e("asdf", "asdf");
 	c.w("asdf", "asdf");
 	c.d("asdf", "asdf");
